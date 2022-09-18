65e91c83feefe545200dca21854da4715daffde9
alex <yinchihkai0909@gmail.com>
2022-09-18 16:03:20 +0800

    adding logo

--- components/Nav.js
+++ components/Nav.js
@@ -1,13 +1,20 @@
 //hooks
 import Link from "next/link";
 
+//components
+import Image from "next/image"; //this allows lazy load
 
 
 const Nav = () => {
   return ( 
     <nav>
       <div className="logo">
-        <h1>Next List App</h1>
+        <Image 
+        src='/Nextjs-logo.svg.png' 
+        alt='next-logo'
+        width={128}
+        height={76.7}
+        />
       </div>
       <Link href='/'>
         <a>Home</a>
